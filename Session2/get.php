b3be7ba599c91ede1003eebbcf08f70b3db9eb22
TuanHA <tuan.ha@tomosia.com>
2021-06-27 10:13:20 +0700

    update

--- Session2/get.php
+++ Session2/get.php
@@ -1,18 +1,24 @@
 <?php
-function getBook($myDB, $author_name = "", $title = "", $year = "", $isbn = "")
+function getBook($myDB, $author_name, $title, $year, $isbn)
 {
-  $author_name = $author_name . "%";
-  $title = $title . "%";
-  $year = $year . "%";
-  $isbn = $isbn . "%";
-  $sql = "SELECT bookid,title,pub_year,isbn,author.name FROM Books inner join Author on books.authorid = author.authorid Where available = 1 and name like '%" . $author_name . "' and title like '%" . $title . "'  and pub_year like '%" . $year . "' and isbn like '%" . $isbn . "' order by title";
-  $result = $myDB->query($sql);
+  $author_name = $author_name ? "%" . $author_name . "%" : "%";
+  $title = $title ? "%" . $title . "%" : "%";
+  $year = $year ? "%" . $year . "%" : "%";
+  $isbn = $isbn ? "%" . $isbn . "%" : "%";
+  $query = "SELECT bookid,title,pub_year,isbn,author.name FROM Books inner join Author on books.authorid = author.authorid Where available = 1 and name like ? and title like ? and pub_year like ? and isbn like ? order by title";
+  $stmt = $myDB->prepare($query);
+  $stmt->bind_param("ssss", $author_name, $title, $year, $isbn);
+  $stmt->execute();
+  $result = $stmt->get_result();
   return $result;
 }
-function getAuthor($myDB, $author_name = "")
+function getAuthor($myDB, $author_name)
 {
-  $author_name = $author_name . "%";
-  $sql = "SELECT Author.authorid,Author.name FROM Author Where name like '%" . $author_name . "' order by name";
-  $result = $myDB->query($sql);
+  $author_name = $author_name ? "%" . $author_name . "%" : "%";
+  $query = "SELECT Author.authorid,Author.name FROM Author Where name like ? order by name";
+  $stmt = $myDB->prepare($query);
+  $stmt->bind_param("s", $author_name);
+  $stmt->execute();
+  $result = $stmt->get_result();
   return $result;
 }
--- Session2/post.php
+++ Session2/post.php
@@ -1,23 +1,33 @@
 <?php
 function addBook($myDB, $author_name, $title, $year, $isbn)
 {
-  $sql = "INSERT INTO `Libary`.`Books` (`authorid`, `title`, `ISBN`, `pub_year`, `available`) select author.authorid, '" . $title . "', '" . $isbn . "', '" . $year . "', 1 from author where author.name like '%" . $author_name . "%';";
-  $myDB->query($sql);
+  $query = "INSERT INTO `Libary`.`Books` (`authorid`, `title`, `ISBN`, `pub_year`, `available`) select author.authorid, ?, ?, ?, 1 from author where author.name like '%?%';";
+  $stmt = $myDB->prepare($query);
+  $stmt->bind_param("ssss", $title, $isbn, $year, $author_name);
+  $stmt->execute();
 }
 function deleteBook($myDB, $bookid)
 {
-  $sql = "DELETE FROM `Libary`.`Books` WHERE (`bookid` = " . $bookid . ");";
-  $myDB->query($sql);
+  $query = "DELETE FROM `Libary`.`Books` WHERE (`bookid` = ?);";
+  $stmt = $myDB->prepare($query);
+  $stmt->bind_param("i", $bookid);
+  $stmt->execute();
 }
 function addAuthor($myDB, $author_name)
 {
-  $sql = "INSERT INTO `Libary`.`Author` (`name`) VALUES ('" . $author_name . "');";
-  $myDB->query($sql);
+  $query = "INSERT INTO `Libary`.`Author` (`name`) VALUES (?);";
+  $stmt = $myDB->prepare($query);
+  $stmt->bind_param("s", $author_name);
+  $stmt->execute();
 }
 function deleteAuthor($myDB, $authorid)
 {
-  $deleteAllBook = "DELETE FROM `Libary`.`Books` WHERE (`authorid` = " . $authorid . ");";
-  $deleteAuthor = "DELETE FROM `Libary`.`Author` WHERE (`authorid` = " . $authorid . ");";
-  $myDB->query($deleteAllBook);
-  $myDB->query($deleteAuthor);
+  $deleteAllBook = "DELETE FROM `Libary`.`Books` WHERE (`authorid` = ?);";
+  $deleteAuthor = "DELETE FROM `Libary`.`Author` WHERE (`authorid` = ?);";
+  $stmt = $myDB->prepare($deleteAllBook);
+  $stmt->bind_param("i", $authorid);
+  $stmt->execute();
+  $stmt = $myDB->prepare($deleteAuthor);
+  $stmt->bind_param("i", $authorid);
+  $stmt->execute();
 }
